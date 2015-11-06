226dbcb9da185cc59d2bf8c79f55d9c6f58d2b71
youbehappy <youbehappy@qq.com>
2015-11-06 07:29:37 -0500

    initiate

--- DirectoryBasedCacheMachenismSimulator/src/Simulator.java
+++ DirectoryBasedCacheMachenismSimulator/src/Simulator.java
@@ -2,19 +2,61 @@ import java.util.LinkedList;
 
 
 public class Simulator {
-	public Simulator(){}
 	
-	int p =0;//The number of processors
-	int n1 = 0;//The size of every l1
-	int n2 = 0;//The size of l2
-	int b = 0;//The size of a block
-	int a1 = 0;//The associativity of l1
-	int a2 = 0;//The associativity of l2;
-	int C = 0;//The number of delay cycles caused by communicating between two nodes(a node consists of a processor and l1 cache)
-	int d = 0;//The number of cycles caused by a l2 hit(The l1 hit is satisfied in the same cycle in which it is issued)
-	int d1 = 0;//The number of cycles caused by a memory access
+	/*
+	 * The number of processors
+	 */
+	int p =0;
+	/*
+	 * The size of every l1
+	 */
+	int n1 = 0;
+	/*
+	 * The size of l2
+	 */
+	int n2 = 0;
+	/*
+	 * The size of a block
+	 */
+	int b = 0;
+	/*
+	 * The associativity of l1
+	 */
+	int a1 = 0;
+	/*
+	 * The associativity of l2;
+	 */
+	int a2 = 0;
+	/*
+	 * The number of delay cycles caused by communicating between two nodes(a node consists of a processor and l1 cache)
+	 */
+	int C = 0;
+	/*
+	 * The number of cycles caused by a l2 hit(The l1 hit is satisfied in the same cycle in which it is issued)
+	 */
+	int d = 0;
+	/*
+	 * The number of cycles caused by a memory access
+	 */
+	int d1 = 0;
 	
 	LinkedList writeBuffer = new LinkedList();//Stores the blocks that are going te be flushed back to memory
+	
+	public Simulator(int p,int n1,int n2 ,int b,int a1,int a2,int C,int d,int d1){
+		this.p=p;
+		this.n1=n1;
+		this.n2=n2;
+		this.b=b;
+		this.a1=a1;
+		this.a2=a2;
+		this.C=C;
+		this.d=d;
+		this.d1=d1;
+		initializeUnits();
+	}
+	void initializeUnits(){
+		
+	}
 	/**
 	 * @param args
 	 */
